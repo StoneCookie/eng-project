fa242a2b552a11c09e4929f3e9b20ef605c89073
Perezhogin Fedor <pravsha63@gmai.com>
2020-07-03 20:34:56 +0300

    finish-site

--- site/js/main.js
+++ site/js/main.js
@@ -9,26 +9,49 @@ $(function () {
 				return 'auto';
 		});
 	});
-	$('.search__btn').on('click', function () {
+	$('a[href^="#"]').click(function () {
+		let headerHeight = $('header').outerHeight();
+		let target = $(this).attr('href');
+		$('html, body').animate({
+			scrollTop: $(target).offset().top - headerHeight - 20
+		}, 500);
+	});
+	$('.search-wrap__btn').on('click', function () {
 		$('.search').toggleClass('search_active')
 	});
 
 	// При клике на кнопку входа
 	$('.account__link:first-of-type').on('click', function () {
 		$('.nav-item:first-child .nav-link').addClass('active') // show active
-		$('.tab-pane:first-child').addClass('show').addClass('active')
+		$('.tab-pane:first-child').addClass('show active')
 	});
 
 	// При клике на кнопку регистрации
 	$('.account__link:last-of-type').on('click', function () {
-		$('.nav-item:nth-child(2) .nav-link').addClass('active')
+		$('.nav-item:last-child .nav-link').addClass('active')
 		$('.tab-pane:last-child').addClass('show active')
 	});
+
+	// При клике на кнопку редактирования
+	$('.main-inner__btn:first-of-type').on('click', function () {
+		$('.nav-item:first-child .nav-link').addClass('active')
+		$('.tab-pane:first-child').addClass('show active')
+	});
+
+	// При клике на кнопку удаления
+	$('.main-inner__btn:last-of-type').on('click', function () {
+		$('.nav-item:last-child .nav-link').addClass('active')
+		$('.tab-pane:last-child').addClass('show active')
+	});
+
+	// Убирает не нужные классы
 	$('.close').on('click', function () {
 		if ($('.modal-dialog').css('display', 'block')) {
 			$('.nav-item .nav-link').removeClass('active')
 			$('.tab-pane').removeClass('show active')
 		}
 	});
-
+	$('.folow_anchor').on('click', function () {
+		$('.folow_anchor').toggleClass('folow_active')
+	})
 });
